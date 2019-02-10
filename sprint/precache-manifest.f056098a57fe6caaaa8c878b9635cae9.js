9808cb294993ddd18f4470cd4ee58e8875292748
Arun <arunmankad@gmail.com>
2019-02-10 09:11:29 +0530

    new react sprint statusapp

---
+++ sprint/precache-manifest.f056098a57fe6caaaa8c878b9635cae9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2b4bc10698cc28c6f238",
+    "url": "/static/js/main.2b4bc106.chunk.js"
+  },
+  {
+    "revision": "7863143719915d6b1f6f",
+    "url": "/static/js/1.78631437.chunk.js"
+  },
+  {
+    "revision": "2b4bc10698cc28c6f238",
+    "url": "/static/css/main.4162c10c.chunk.css"
+  },
+  {
+    "revision": "1cb2e83c88247a273b795936d2fea21a",
+    "url": "/index.html"
+  }
+];
